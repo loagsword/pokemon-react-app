b4d45e3b41bb3dd4456aa2388e867ee19f1606a4
Lolu Aguda <42425277+loagsword@users.noreply.github.com>
2020-04-11 11:06:25 -0400

    feature: project refactor + added api queries

--- src/containers/PokemonsContainer.js
+++ src/containers/PokemonsContainer.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { useQuery } from '@apollo/react-hooks';
 import { Pokemon } from '../components/Pokemon';
- 
+import { GET_POKEMONS } from '../graphql/get-pokemons';
+
 
 export function PokemonsContainer() {
     const { data: { pokemons = [] } = {} } = useQuery(GET_POKEMONS, {
