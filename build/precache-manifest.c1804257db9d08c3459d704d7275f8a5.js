0e4a101dccd0df464543da734e0ec0516fc56a26
Lolu Aguda <42425277+loagsword@users.noreply.github.com>
2020-04-11 13:21:47 -0400

    feature: added build

---
+++ build/precache-manifest.c1804257db9d08c3459d704d7275f8a5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c04e8b468271fe916aa915d54c0c9b8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e9cae9abb43dba09af3a",
+    "url": "/static/css/main.b882f15a.chunk.css"
+  },
+  {
+    "revision": "0c0afe9ab804713fff9f",
+    "url": "/static/js/2.5a8b8696.chunk.js"
+  },
+  {
+    "revision": "501f379103cbfcb5ff11211dd39a28a2",
+    "url": "/static/js/2.5a8b8696.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9cae9abb43dba09af3a",
+    "url": "/static/js/main.6742bd3f.chunk.js"
+  },
+  {
+    "revision": "6dddc7a6df6de310690a",
+    "url": "/static/js/runtime-main.5466fafa.js"
+  }
+]);
